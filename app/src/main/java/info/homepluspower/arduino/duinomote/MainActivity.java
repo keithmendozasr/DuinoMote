7923fb581ff479cc1fe4d52bd9ed5afb262adb2b
Keith Mendoza <pantherse@gmail.com>
2014-12-27 23:15:19 -0800

    Re-did base QUI layout

--- app/src/main/java/info/homepluspower/arduino/duinomote/MainActivity.java
+++ app/src/main/java/info/homepluspower/arduino/duinomote/MainActivity.java
@@ -17,13 +17,13 @@
 package info.homepluspower.arduino.duinomote;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.SeekBar;
 
-public class MainActivity extends ActionBarActivity implements SeekBar.OnSeekBarChangeListener {
+public class MainActivity extends Activity {
     private final static String LogTag = MainActivity.class.getName();
 
     @Override
@@ -31,9 +31,6 @@ public class MainActivity extends ActionBarActivity implements SeekBar.OnSeekBar
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-
-        ((SeekBar)findViewById(R.id.leftSlider)).setOnSeekBarChangeListener(this);
-        ((SeekBar)findViewById(R.id.rightSlider)).setOnSeekBarChangeListener(this);
     }
 
     @Override
@@ -43,44 +40,4 @@ public class MainActivity extends ActionBarActivity implements SeekBar.OnSeekBar
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-        switch(seekBar.getId())
-        {
-            case R.id.leftSlider:
-                Log.v(LogTag, "Left slider moving to " + Integer.toString(progress));
-                break;
-            case R.id.rightSlider:
-                Log.v(LogTag, "Right slider moving to " + Integer.toString(progress));
-                break;
-            default:
-                Log.w(LogTag, "Got progress from " + Integer.toString(seekBar.getId()));
-                break;
-        }
-    }
-
-    @Override
-    public void onStartTrackingTouch(SeekBar seekBar) {
-        //do nothing
-    }
-
-    @Override
-    public void onStopTrackingTouch(SeekBar seekBar) {
-        //do nothing
-    }
 }
