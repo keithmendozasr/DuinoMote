f7169c57d3b5a4871c875176fa5e40fbad72a915
Keith Mendoza <pantherse@gmail.com>
2015-01-01 21:33:52 -0800

    Modified steering to seek bar

--- app/src/main/java/info/homepluspower/arduino/duinomote/MainActivity.java
+++ app/src/main/java/info/homepluspower/arduino/duinomote/MainActivity.java
@@ -19,34 +19,49 @@ package info.homepluspower.arduino.duinomote;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.GridLayout;
-import android.widget.Switch;
 import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Button;
+import android.widget.SeekBar;
 
 public class MainActivity extends Activity {
     private final static String LogTag = MainActivity.class.getName();
+    private Button speedUpBtn, stopBtn, speedDownBtn;
+    private SeekBar steerBar;
+    private EditText robotIPText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        speedUpBtn      = (Button) findViewById(R.id.speedUpBtn);
+        stopBtn         = (Button) findViewById(R.id.stopBtn);
+        speedDownBtn    = (Button) findViewById(R.id.speedDownBtn);
+        steerBar        = (SeekBar) findViewById(R.id.steerBar);
+        robotIPText     = (EditText) findViewById(R.id.robotIPText);
+    }
+
+    private void setControlVisibility(boolean visible)
+    {
+        int visibility = (visible ? View.VISIBLE : View.INVISIBLE);
+
+        speedUpBtn.setVisibility(visibility);
+        stopBtn.setVisibility(visibility);
+        speedDownBtn.setVisibility(visibility);
+        steerBar.setVisibility(visibility);
+
+        robotIPText.setEnabled(!visible);
     }
 
     //Handle "Connect" being switched
     public void onActivateClicked(View v)
     {
-        GridLayout controls = (GridLayout) findViewById(R.id.controlLayout);
-
         Switch s = (Switch)v;
-        if(s.isChecked()) {
-            controls.setVisibility(View.VISIBLE);
-            ((EditText)findViewById(R.id.robotIPText)).setEnabled(false);
-        }
+        if(s.isChecked())
+            setControlVisibility(true);
         else
-        {
-            controls.setVisibility(View.INVISIBLE);
-            ((EditText)findViewById(R.id.robotIPText)).setEnabled(true);
-        }
+            setControlVisibility(false);
     }
 }
